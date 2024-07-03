036e5e7c017b34a12d9e3c2261f6c662352bdaaf
altyon_get <shubhamojha4444@gmail.com>
2024-07-03 16:15:07 +0530

    celery setup

--- cnext_apitracker_backend/celery.py
+++ cnext_apitracker_backend/celery.py
@@ -1,10 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cnext_api_tracker_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cnext_apitracker_backend.settings')
 app = Celery('cnext_apitracker_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    'hit-apis-every-hour':{
+        'task': 'api.tasks.hit_apis_and_log',
+        'schedule': crontab(minute='*')
+    }
+}
+
+
 app.autodiscover_tasks()
 
 @app.task(bind=True)
