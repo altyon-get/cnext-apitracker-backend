85163eef6a30697e780dc5671a398183ab6adad5
altyon_get <shubhamojha4444@gmail.com>
2024-07-07 23:07:50 +0530

    removded serializers from api

--- cnext_apitracker_backend/celery.py
+++ cnext_apitracker_backend/celery.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cnext_apitracker_backend.settings')
@@ -11,11 +10,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'hit-apis-every-hour':{
+    'hit-apis-every-hour': {
         'task': 'api.tasks.hit_apis_and_log',
         'schedule': crontab(minute='*/3')
     }
 }
+
 
 @app.task(bind=True)
 def debug_task(self):
