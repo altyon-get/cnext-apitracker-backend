9482027975c661657197e46aa7555d68b751843f
anantcareers <163998124+anantcareers@users.noreply.github.com>
2024-07-22 13:11:10 +0530

    Create run.bash

---
+++ cnext_apitracker_backend/deploy/run.bash
@@ -0,0 +1,7 @@
+mkdir -p /var/log/python
+mkdir -p /home/ubuntu/main/cnext-apitracker-backend/sitemap
+touch /var/log/python/debug.log
+nohup python manage.py runserver > /var/log/python/debug.log 2>&1 &
+nohup celery -A cnext_apitracker_backend worker --loglevel=info &
+nohup celery -A cnext_apitracker_backend beat --loglevel=info &
+tail -f /var/log/python*
